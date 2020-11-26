23579e85a1ca12ce4cd64458bef46672e8c20fd8
Anderson Menezes <anderson.menezes@objective.com.br>
2020-11-26 08:32:48 -0300

    refatoração testes multiples

--- src/exercises/multiples/multiples.test.ts
+++ src/exercises/multiples/multiples.test.ts
@@ -1,44 +1,62 @@
 import isMultipleOf3And5 from "./is-multiple-of-three-and-five";
 import isMultipleOf3Or5 from "./is-multiple-of-three-or-five";
 import isMultipleOf3Or5And7 from "./is-multiple-of-three-or-five-and-seven";
-import multiples  from "./multiples";
+import multiples from "./multiples";
 
 describe("Testes exercício 1", () => {
-  test("soma dos valores múltiplos de 3 ou 5 abaixo de 1000", () => {
-    expect(multiples().threeOrFive).toBe(233168);
+  describe("isMultipleOf3And5", () => {
+    test(`deve retornar 'true' se o número for múltiplo de 3 e 5`, () => {
+      expect(isMultipleOf3And5(15)).toBe(true);
+
+      expect(isMultipleOf3And5(45)).toBe(true);
+    });
+
+    test(`deve retornar 'false' se o número não for múltiplo de 3 e 5`, () => {
+      expect(isMultipleOf3And5(9)).toBe(false);
+
+      expect(isMultipleOf3And5(5)).toBe(false);
+    });
   });
 
-  test("soma dos valores múltiplos de 3 e 5 abaixo de 1000", () => {
-    expect(multiples().threeAndFive).toBe(33165);
+  describe("isMultipleOf3Or5", () => {
+    test(`deve retornar 'true' se o número for múltiplo de 3 ou 5`, () => {
+      expect(isMultipleOf3Or5(9)).toBe(true);
+
+      expect(isMultipleOf3Or5(5)).toBe(true);
+    });
+
+    test(`deve retornar 'false' se o número não for múltiplo de 3 ou 5`, () => {
+      expect(isMultipleOf3Or5(4)).toBe(false);
+
+      expect(isMultipleOf3Or5(14)).toBe(false);
+    });
   });
 
-  test("soma dos valores múltiplos de (3 ou 5) e 7 abaixo de 1000", () => {
-    expect(multiples().threeOrFiveAndSeven).toBe(33173);
+  describe("isMultipleOf3Or5And7", () => {
+    test(`deve retornar 'true' se o número for múltiplo de (3 ou 5) e 7`, () => {
+      expect(isMultipleOf3Or5And7(21)).toBe(true);
+
+      expect(isMultipleOf3Or5And7(42)).toBe(true);
+    });
+
+    test(`deve retornar 'false' se o número não for múltiplo de (3 ou 5) e 7`, () => {
+      expect(isMultipleOf3Or5And7(7)).toBe(false);
+
+      expect(isMultipleOf3Or5And7(14)).toBe(false);
+    });
   });
 
-  test("verifica se a função retorna corretamente um número múltiplo de 3 ou 5", () => {
-    expect(isMultipleOf3Or5(9)).toBe(true);
+  describe("multiples", () => {
+    test("soma dos valores múltiplos de 3 ou 5 abaixo de 1000", () => {
+      expect(multiples().threeOrFive).toBe(233168);
+    });
 
-    expect(isMultipleOf3Or5(5)).toBe(true);
+    test("soma dos valores múltiplos de 3 e 5 abaixo de 1000", () => {
+      expect(multiples().threeAndFive).toBe(33165);
+    });
 
-    expect(isMultipleOf3Or5(4)).toBe(false);
-  });
-
-  test("verifica se a função retorna corretamente um número múltiplo de 3 e 5", () => {
-    expect(isMultipleOf3And5(9)).toBe(false);
-
-    expect(isMultipleOf3And5(5)).toBe(false);
-
-    expect(isMultipleOf3And5(15)).toBe(true);
-  });
-
-  test("verifica se a função retorna corretamente um número múltiplo de (3 ou 5) e 7", () => {
-    expect(isMultipleOf3Or5And7(7)).toBe(false);
-    
-    expect(isMultipleOf3Or5And7(14)).toBe(false);
-    
-    expect(isMultipleOf3Or5And7(21)).toBe(true);
-    
-    expect(isMultipleOf3Or5And7(42)).toBe(true);
+    test("soma dos valores múltiplos de (3 ou 5) e 7 abaixo de 1000", () => {
+      expect(multiples().threeOrFiveAndSeven).toBe(33173);
+    });
   });
 });
