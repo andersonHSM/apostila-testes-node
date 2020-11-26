837c01f34f581b7e1cf558032344bdeca5649d7b
Anderson Menezes <anderson.menezes@objective.com.br>
2020-11-26 08:21:21 -0300

    refatoração nos estes de happy number

--- src/exercises/happy-number/happy-number.test.ts
+++ src/exercises/happy-number/happy-number.test.ts
@@ -3,33 +3,41 @@ import happyNumber from "./happy-number";
 import verifyIfHappyNumber from "./verify-if-happy-number";
 
 describe("Teste dos exercícios 2", () => {
-  test(`deve retornar 'true' caso valor informado seja '1'`, () => {
-    expect(happyNumber(1)).toBe(true);
+  describe("happyNumber", () => {
+    test(`deve retornar 'true' caso valor informado seja '1'`, () => {
+      expect(happyNumber(1)).toBe(true);
+    });
+
+    test(`deve retornar 'true' se o número for feliz`, () => {
+      expect(happyNumber(7)).toBe(true);
+
+      expect(happyNumber(10)).toBe(true);
+    });
+
+    test(`deve retornar 'false' se o número for feliz`, () => {
+      expect(happyNumber(8)).toBe(false);
+    });
   });
 
-  test(`deve retornar a soma dos quadrados dos dígitos do número informado`, () => {
-    expect(calculateNumberDigitsPowSum(8)).toEqual(64);
+  describe("calculateNumberDigitsPowSum", () => {
+    test(`deve retornar a soma dos quadrados dos dígitos do número informado`, () => {
+      expect(calculateNumberDigitsPowSum(8)).toEqual(64);
 
-    expect(calculateNumberDigitsPowSum(12)).toEqual(5);
+      expect(calculateNumberDigitsPowSum(12)).toEqual(5);
 
-    expect(calculateNumberDigitsPowSum(21)).toEqual(5);
+      expect(calculateNumberDigitsPowSum(21)).toEqual(5);
 
-    expect(calculateNumberDigitsPowSum(25)).toEqual(29);
+      expect(calculateNumberDigitsPowSum(25)).toEqual(29);
+    });
   });
 
-  test(`deve verificar se o número atual não viola a regra para um númeo feliz`, () => {
-    expect(verifyIfHappyNumber([1, 15, 23, 19], 12)).toBe(true);
+  describe("verifyIfHappyNumber", () => {
+    test(`deve retornar 'true' se o número não violar a regra de um número feliz`, () => {
+      expect(verifyIfHappyNumber([1, 15, 23, 19], 12)).toBe(true);
+    });
 
-    expect(verifyIfHappyNumber([1, 15, 23, 19], 19)).toBe(false);
-  });
-
-  test("deve verificar se um dado número é feliz ou não", () => {
-    expect(happyNumber(7)).toBe(true);
-
-    expect(happyNumber(1)).toBe(true);
-
-    expect(happyNumber(8)).toBe(false);
-
-    expect(happyNumber(10)).toBe(true);
+    test(`deve retornar 'false' se o número não violar a regra de um número feliz`, () => {
+      expect(verifyIfHappyNumber([1, 15, 23, 19], 19)).toBe(false);
+    });
   });
 });
